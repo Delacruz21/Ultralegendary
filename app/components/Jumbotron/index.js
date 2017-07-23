ba08be816c6f425d31c1cdd9dbd69b1fd57fb6e0
CS\MichaelD <michaeld@locus.net>
2017-07-23 17:04:13 -0400

    added functions to hande sign in pages

--- app/components/Jumbotron/index.js
+++ app/components/Jumbotron/index.js
@@ -5,24 +5,58 @@
 */
 
 import React from 'react';
-
-
+import { browserHistory } from 'react-router';
+import IconButton from '../IconButton';
 import styles from './styles.css';
 
-function Jumbotron() {
-  return (
-    <div className={styles.jumbotron}>
-      <div className="jumbotron text-center">
-        <h1>Ultra Legendary</h1>
+class Jumbotron extends React.Component {
+  static propTypes = {
+    email: React.PropTypes.string,
+  }
+  // function for redirecting to signup page
+  signIn() {
+    return browserHistory.push('/signup');
+  }
+  //  display sign in page
+  displaySignIn() {
+    return (
+      <div>
         <p>Please Sign In</p>
         <div className={styles.wrapper}>
-          <button type="button" className={styles.button}>
-            <span className="glyphicon glyphicon-plus" aria-hidden="true"></span>
-          </button>
+          <IconButton
+            type="button"
+            icon="glyphicon glyphicon-plus"
+            buttonClass={styles.button}
+            onClick={this.signIn}
+          />
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  // display signed in page div
+  displaySignedIn() {
+    return (
+      <div>
+        <p>Hi, <span style={{ textTransform: 'lowercase', color: 'red', background: 'lightblue' }}>{this.props.email}</span> and
+          Thanks for checking out my site!
+        </p>
+        <p><var>I used reselect to combine selectors and took email held in state set by redux
+        to pass down to props for it to be displayed on this navigation page!
+        </var></p>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className={styles.jumbotron}>
+        <div className="jumbotron text-center">
+          <h1>Ultra Legendary</h1>
+          {this.props.email ? this.displaySignedIn() : this.displaySignIn()}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Jumbotron;

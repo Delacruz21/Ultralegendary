f55ce8c5500f9aa5e088464d55d60bb4db38643c
CS\MichaelD <michaeld@locus.net>
2017-07-23 17:02:01 -0400

    fixed small UI change

--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -10,21 +10,31 @@ import Jumbotron from '../Jumbotron';
 import Panel from '../Panel';
 // import styles from './styles.css';
 
-function Navigation() {
+function Navigation({ email, children }) {
   return (
     <div className="container-fluid">
       <nav className="navbar navbar-default navbar-fixed-top">
         <AppBar />
       </nav>
-      <Jumbotron />
-      <Panel
-        panelHeading="Welcome! My name is Michael De La Cruz"
-        panelContent="I hope to give you an insight into my work, knowledge, and interests. Having this site allows me to express myself using HTML5, CSS3, and JavaScript"
-        panelQuote="Be who you are and say what you feel, because those who mind don't matter and those who matter don't mind."
-        quoteAuthor="Dr. Seuss"
-      />
+      <div className="row-fluid">
+        <Jumbotron
+          email={email}
+        />
+        <Panel
+          panelHeading="Welcome! My name is Michael De La Cruz"
+          panelContent="I hope to give you an insight into my work, knowledge, and interests. Having this site allows me to express myself using HTML5, CSS3, and JavaScript"
+          panelQuote="Be who you are and say what you feel, because those who mind don't matter and those who matter don't mind."
+          quoteAuthor="Dr. Seuss"
+        />
+        {children}
+      </div>
     </div>
   );
 }
+
+Navigation.propTypes = {
+  email: React.PropTypes.string,
+  children: React.PropTypes.element,
+};
 
 export default Navigation;
